edbdcfee85cdecdae36602723f49ec4e85e90ea9
aniket-patil7075 <akp7075@gmail.com>
2024-08-24 13:16:02 +0530

    Done upto WhyChoose compo

--- src/assets/WhyChoose.jsx
+++ src/assets/WhyChoose.jsx
@@ -5,33 +5,33 @@ import Col from 'react-bootstrap/Col';
 
 function WhyChoose() {
   return (
-    <div className='bg-body-tertiay'>
+    <div className='bg-body-tertiay mt-5'>
       <Container>
         <Row >
-            <Col className='p-4'>
+            <Col className='p-4 mt-5'>
                 <h1>Why Choose Us</h1>
                 <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
                 <table>
-                    <tr>
-                        <td>
-                            <img src=''/>
+                    <tr className=''>
+                        <td className='pt-4 pe-2'>
+                            <img src='./truck.svg'/>
                             <h6>Fast & Free Shipping</h6>
                             <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
                         </td>
-                        <td>
-                            <img src=''/>
+                        <td className='pt-4 pe-2'>
+                            <img src='./bag.svg'/>
                             <h6>Easy to Shop</h6>
                             <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
                         </td>
                     </tr>
                     <tr>
-                        <td>
-                            <img src=''/>
+                        <td className='pt-4 pe-2'>
+                            <img src='./support.svg'/>
                             <h6>24/7 Support</h6>
                             <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
                         </td>
-                        <td>
-                            <img src=''/>
+                        <td className='pt-4  pe-2'>
+                            <img src='./return.svg'/>
                             <h6>Hassle Free Returns</h6>
                             <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
                         </td>
@@ -39,10 +39,10 @@ function WhyChoose() {
                 </table>
             </Col>
             <Col>
-                <div>
+                <div className='position-relative '>
                     <img src='./dots-yellow.svg' />
-                    <div>
-                        <img src='./why-choose-us-img.jpg' />
+                    <div className='position-absolute top-50 end-0'>
+                        <img src='./why-choose-us-img.jpg' className='rounded rounded-5 ' style={{ width: "26rem" ,height:"520px" }}/>
                     </div>
                 </div>
             </Col>
