badd7ecfdecbad59e4cecd3df5445dee660a0946
aniket-patil7075 <akp7075@gmail.com>
2024-08-31 10:18:01 +0530

    Add new component Shipping

---
+++ src/assets/Shipping.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+
+function Shipping() {
+  return (
+    <div className="py-5">
+      <table className="">
+        <tr className="">
+          <td className="pt-4 pe-3 px-5">
+            <img src="./truck.svg" />
+            <h6 className="pt-3">Fast & Free Shipping</h6>
+            <p className="text-secondary">
+              Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+              velit. Aliquam vulputate.
+            </p>
+          </td>
+          <td className="pt-4 pe-3">
+            <img src="./bag.svg" />
+            <h6 className="pt-3">Easy to Shop</h6>
+            <p className="text-secondary">
+              Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+              velit. Aliquam vulputate.
+            </p>
+          </td>
+          <td className="pt-4 pe-3">
+            <img src="./support.svg" />
+            <h6 className="pt-3">24/7 Support</h6>
+            <p className="text-secondary">
+              Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+              velit. Aliquam vulputate.
+            </p>
+          </td>
+          <td className="pt-4  pe-5 ">
+            <img src="./return.svg" />
+            <h6 className="pt-3">Hassle Free Returns</h6>
+            <p className="text-secondary">
+              Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+              velit. Aliquam vulputate.
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <td className="pt-4 pe-3 px-5">
+            <img src="./truck.svg" />
+            <h6 className="pt-3">Fast & Free Shipping</h6>
+            <p className="text-secondary">
+              Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+              velit. Aliquam vulputate.
+            </p>
+          </td>
+          <td className="pt-4 pe-3">
+            <img src="./bag.svg" />
+            <h6 className="pt-3">Easy to Shop</h6>
+            <p className="text-secondary">
+              Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+              velit. Aliquam vulputate.
+            </p>
+          </td>
+          <td className="pt-4 pe-3">
+            <img src="./support.svg" />
+            <h6 className="pt-3">24/7 Support</h6>
+            <p className="text-secondary">
+              Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+              velit. Aliquam vulputate.
+            </p>
+          </td>
+          <td className="pt-4  pe-5 ">
+            <img src="./return.svg" />
+            <h6 className="pt-3">Hassle Free Returns</h6>
+            <p className="text-secondary">
+              Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+              velit. Aliquam vulputate.
+            </p>
+          </td>
+        </tr>
+      </table>
+      
+    </div>
+  );
+}
+
+export default Shipping;
